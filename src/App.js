e2a83c3cc45b41586e874562e2bf89f36a0afdd0
TAVERNIER Thibaut <thibaut.tavernier@3ds.com>
2023-07-26 17:34:51 +0200

    Before add login request

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
@@ -6,22 +6,19 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import DataObjectIcon from '@mui/icons-material/DataObject';
 
 const theme = createTheme({
   palette: {
- 
-      primary: {
-        main: '#0971f1',
-        darker: '#053e85',
-      },
-      Platform3DS : {
-        main: '#235581',
-        contrastText: '#fff',
-      },
+
+    primary: {
+      main: '#0971f1',
+      darker: '#053e85',
+    },
+    Platform3DS: {
+      main: '#235581',
+      contrastText: '#fff',
+    },
   },
 });
 
@@ -29,19 +26,11 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static" color="Platform3DS">
+        <AppBar position="static" color="Platform3DS" >
           <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            >
-              <MenuIcon />
-            </IconButton>
+            <DataObjectIcon />
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              News
+              3DExperience
             </Typography>
             <Button color="inherit">Login</Button>
           </Toolbar>
